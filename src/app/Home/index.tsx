9cc3742a221c664f6c577dcd32df28a429b8f9a8
Glauco Gamero <glauco.h.gamero@gmail.com>
2025-10-25 08:13:19 -0300

    finalizado App

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,143 @@
+import {  Alert, FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { FilterStatus } from '@/types/FilterStatus';
+import { Item } from '@/components/Item';
+import { useEffect, useState } from 'react';
+import { itemsStorage, ItemStorage } from '@/storage/itemsStorage';
+
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
+
+
+export  function Home() {
+  const [filter,setFilter] = useState(FilterStatus.PENDING)
+  const [description, setDescription] = useState("")
+  const [items, setItems] = useState<ItemStorage[]>([])
+
+  async function handleAdd(){
+    if(!description.trim()){
+      return Alert.alert("Descrição inválida","Por favor insira uma descrição válida para o item.")
+    }
+
+    const newItem = {
+      id: Math.random().toString(36).substring(2),
+      description,
+      status: FilterStatus.PENDING
+    }
+    
+    await itemsStorage.add(newItem)
+    await itemsByStatus()
+    
+    setFilter(FilterStatus.PENDING)
+    Alert.alert("Adicionado",`Adicionado ${description}`)
+    setDescription("")
+  }
+  async function itemsByStatus(){
+    try{
+      const response = await itemsStorage.getByStatus(filter)
+      setItems(response)
+      
+    }catch(error){
+      console.log(error)
+      Alert.alert("Erro","Não foi possível carregar os itens.") 
+    }
+  }
+  async function HandleRemove(id: string){
+    try{
+      await itemsStorage.remove(id)
+      await itemsByStatus()
+    }catch(error){
+      console.log(error)
+      Alert.alert("Erro","Não foi possível remover o item.") 
+    }
+  }
+  function handleClear(){
+      Alert.alert("Limpar itens","Tem certeza que deseja limpar todos os itens?",[
+        {
+          text: "Não",
+          style: "cancel"
+        },
+        {
+          text: "Sim",
+          onPress: () => onClear()
+        }
+      ])
+  }
+  async function onClear() {
+    try{
+      await itemsStorage.clear()
+      setItems([])
+
+    }catch(error){
+      console.log(error)
+      Alert.alert("Erro","Não foi possível limpar os itens.") 
+    }
+  }
+  async function handleToggleItemStatus(id: string){
+    try{
+
+      await itemsStorage.toggleStatus(id)
+      await itemsByStatus()
+
+    }catch(error){
+      console.log(error)
+      Alert.alert("Erro","Não foi possível atualizar o status do item.") 
+    }
+  }
+  useEffect(() => {
+    itemsByStatus()
+  },[filter])
+  return (
+    <View style={styles.container}>
+      <Image style={styles.logo} 
+        source={(require("@/assets/logo.png"))} />
+      <View style={styles.form}>
+        <Input 
+          placeholder='O que você precisa comprar'
+          onChangeText={setDescription}
+          value={description}
+        />
+        <Button title="Adicionar" 
+          onPress={handleAdd}
+        />
+      </View>
+      
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) =>(
+            <Filter 
+              key={status} 
+              status={status}
+              isActive = {status === filter}
+              onPress={() => setFilter(status)}
+            />
+          ))}
+          <TouchableOpacity 
+            style={styles.clearButton}
+            onPress={handleClear}
+            >
+              <Text 
+                style={styles.clearText}>Limpar</Text>
+            </TouchableOpacity>
+        </View>
+        <FlatList
+          data={items}
+          keyExtractor={item => item.id}
+          renderItem={({item}) => (
+              <Item
+                data={item}
+                onStatus={() => handleToggleItemStatus(item.id)}
+                onRemove={() => HandleRemove(item.id)}
+              />)}
+          showsVerticalScrollIndicator={false}
+          ItemSeparatorComponent={() => <View style={styles.separator}/>}
+          contentContainerStyle={styles.listContent}
+          ListEmptyComponent={() => <Text style={styles.emptyText}>Nenhum item aqui</Text>}
+        />
+      </View>
+    </View>
+  );
+}
